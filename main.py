65b1a3cd45119b729da367631d12c6dea7c0ebd8
Szagha02 <78236463+Szagha02@users.noreply.github.com>
2023-11-10 12:53:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ SAVE_DIRECTORY = "/Users/mertryankiroglu/Desktop/simple-HRNet/session/photos"
 # Load model
 model = SimpleHRNet(48, 17, "/Users/mertryankiroglu/Desktop/simple-HRNet/models_/detectors/yolo/weights/pose_hrnet_w48_384x288.pth")
 
-def extract_joints_from_image(image_path):
+def extract_joints_from_image(image_path, person_tracker, unique_id_counter):
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
 
@@ -25,9 +25,19 @@ def extract_joints_from_image(image_path):
             x, y = joints[p, j, 0], joints[p, j, 1]
             joint_name = joint_names[j]
             print(f"{joint_name}: (x={y:.2f}, y={x:.2f})")
+        
+        # Assign a unique ID to the person and log it
+        if person_tracker.get(image_path) is None:
+            person_tracker[image_path] = unique_id_counter
+            print(f"Assigned ID {unique_id_counter} to Person in {image_path}")
+            unique_id_counter += 1
+
         print("-----")
+    return unique_id_counter
 
 def capture_image():
+    unique_id_counter = 0
+    person_tracker = {}  # To track unique IDs per image
     # Open the webcam (0 is the default camera, change the number if you have multiple cameras)
     cap = cv2.VideoCapture(0)
 
@@ -47,11 +57,13 @@ def capture_image():
             filename = os.path.join(SAVE_DIRECTORY, f"captured_image_{count}.jpg")
             cv2.imwrite(filename, frame)
 
-            # Extract joints from the saved image
-            extract_joints_from_image(filename)
+            # Extract joints from the saved image and get the assigned person ID
+            person_id = extract_joints_from_image(filename, person_tracker, unique_id_counter)
 
-            # Display the frame
-            cv2.imshow('Captured Image', frame)
+            # Display the frame with the assigned person ID
+            frame_with_id = frame.copy()
+            cv2.putText(frame_with_id, f"Person ID: {person_id}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.imshow('Captured Image', frame_with_id)
 
             # Wait for 1 second or until 'q' is pressed
             key = cv2.waitKey(1000)  # waits for 1 second
